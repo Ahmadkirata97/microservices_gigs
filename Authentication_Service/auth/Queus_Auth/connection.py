9a6a8d426fe7bda13517a83a6157e5ccf56ef68a
unknown <ahmad.aldon@outlook.com>
2024-06-12 12:16:22 +0300

    Check Connection to RabbitMQ

---
+++ Authentication_Service/auth/Queus_Auth/connection.py
@@ -0,0 +1,16 @@
+import pika 
+import dotenv
+from dotenv import load_dotenv
+from Helper_Auth.logHandler import logger
+import os 
+
+load_dotenv('.env')
+
+def checkRabbitConnection():
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.getenv('RABBITMQ_URL')))
+        logger.info(f"Auth Service is Connected to RabbitMQ checkRabbitConnection() {connection}")
+        connection.close()
+    except pika.exceptions.AMQPConnectionError as err:
+        str_err = str(err)
+        logger.error(f"Error connecting to RabbitMQ checkRabbitConnection() {str_err}")

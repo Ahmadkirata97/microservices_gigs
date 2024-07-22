d155d78bb38e42ae8d52f6986c5ab316d8fe867d
unknown <ahmad.aldon@outlook.com>
2024-07-22 16:10:34 +0300

    check elastic connection

---
+++ 6-Chat_Service/Chat/Source/Helper_Chat/elastic.py
@@ -0,0 +1,25 @@
+from Helper_Chat.logHandler import logger
+from elasticsearch import Elasticsearch
+import os 
+import logging
+
+
+class ElasticSearch():
+    client = Elasticsearch(os.getenv('ELASTIC_SEARCH_URL'))
+
+
+    def checkConnection(self):
+        print("Checking Elasticsearch Connection")
+        is_connected = False
+        while not is_connected:
+            try:
+                logger.info("Chat Server is connected to ElasticSearch")
+                logger.info(f"ElasticSearch Status is {self.client.cluster.health()}")
+                is_connected = True
+            except Exception as err:
+                logging.info("Connection to ElasticSearch failed, Retrying...")
+                logging.error(f"Chat Service checkConnection() Function error: {str(err)}")
+
+
+
+elastic_connection = ElasticSearch()

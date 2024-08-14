b3f448c8d7768914f8f95ef75e1508dd0dce6c37
unknown <ahmad.aldon@outlook.com>
2024-08-14 21:06:35 +0300

    Update Server to handle Services Routes

--- 1-API_Gateway/api_gateway/server.py
+++ 1-API_Gateway/api_gateway/server.py
@@ -6,27 +6,32 @@ from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_compress import Compress
+from redis import Redis, StrictRedis
 from werkzeug.middleware.proxy_fix import ProxyFix
 from datetime import timedelta
 from logHandler import logger
 from elastic import elastic_connection
-from Routes_API.auth_service import *
-from dotenv import load_dotenv
 from Routes_API.auth_service import auth_blue_print
+from dotenv import load_dotenv
 from Routes_API.buyer_service import buyer_blue_print
 from Routes_API.seller_service import seller_blueprint
-import os
+from Routes_API.gig_service import gig_blue_print
+from Routes_API.chat_service import chat_blue_print
+from Routes_API.order_service import order_blue_print
+from Routes_API.review_service import review_blue_print
+from Sockets_Api.socket import chat_io, order_io
 import threading
+import os
+
 
 
 
 load_dotenv('.env')
 app = Flask(__name__)
-# jwt = JWTManager(app)
+jwt = JWTManager(app)
 
 class gateWayServer(Resource):
 
-    
 
     def __init__(self):
         self.app = app
@@ -41,24 +46,31 @@ class gateWayServer(Resource):
         print("Server has started..")
         elastic_connection_thred = threading.Thread(target=self.startElasticSearch)
         elastic_connection_thred.start()
+        self.createSocketIo()
         self.app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=True)
         
         
 
     def securityMiddleware(self):
         try:
-            ## Must Use Talisman and CSRFPROTECT
-            
-
+            ## Must Use Talisman and CSRFPROTECT     
             self.api.app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
             self.api.app.config['JWT_SECRET_KEY'] = os.getenv('JWT_TOKEN')
+            print(f"JWT Token is {os.getenv('JWT_TOKEN')}")
+            self.api.app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
             self.api.app.wsgi_app = ProxyFix(self.app.wsgi_app) # To tell Flask app that its running behind a proxy
             self.api.app.config['JWT_TOKEN_LOCATION'] = ['cookies'] # To save jwt auth token into cookies 
+            self.api.app.config['JWT_ACCESS_CSRF_HEADER_NAME'] = 'X-CSRF-TOKEN'
+            self.api.app.config['JWT_ACCESS_CSRF_FIELD_NAME'] = 'csrf'
             self.api.app.config['JWT_COOKIE_SECURE'] = False # Only allow JWT cookies to be sent over https. In production, this should likely be True
             self.api.app.config['SESSION_COOKIE_NAME'] = 'session'
+            self.api.app.config['JWT_COOKIE_CSRF_PROTECT'] = True 
+            self.api.app.config['JWT_CSRF_METHODS'] = ['POST', 'PUT', 'PATCH', 'DELETE']  # Specify methods that require CSRF protection
+            self.api.app.config['JWT_CSRF_CHECK_FORM'] = True  # Enable CSRF form check
             self.api.app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
             # self.app['SESSION_COOKIE_SECURE'] = { Should Be modified Later }  # Browsers will only send cookies with requests over HTTPS if the cookie is marked “secure”. The application must be served over HTTPS for this to make sense
-            cors = CORS(self.api.app)  # Allows users to make authenticated requests. If true, injects the Access-Control-Allow-Credentials header in responses. This allows cookies and credentials to be submitted across domains.
+            cors = CORS(self.api.app, origins='')
+            # Allows users to make authenticated requests. If true, injects the Access-Control-Allow-Credentials header in responses. This allows cookies and credentials to be submitted across domains.
             logger.info("flask app security Middleware() is initialized")
         except Exception as error:
             str_error = str(error)
@@ -86,3 +98,15 @@ class gateWayServer(Resource):
         self.app.register_blueprint(auth_blue_print)
         self.app.register_blueprint(seller_blueprint)
         self.app.register_blueprint(buyer_blue_print)
+        self.app.register_blueprint(gig_blue_print)
+        self.app.register_blueprint(chat_blue_print)
+        self.app.register_blueprint(order_blue_print)
+        self.app.register_blueprint(review_blue_print)
+
+
+    def createSocketIo(self):
+      print(f"Chat Service Url is : {os.getenv('MESSAGE_BASE_URL')}")
+      chat_io.connect(url=os.getenv('MESSAGE_BASE_URL'))
+      order_io.connect(url=os.getenv('ORDER_BASE_URL'))
+
+    
